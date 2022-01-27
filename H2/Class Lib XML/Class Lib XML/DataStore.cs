580096e25be7a063926c95f3cd9e5a70a6265710
Mathias Holst <holsthategaming@gmail.com>
2022-01-27 15:54:23 +0100

    Push af H2 projekter/filer

---
+++ H2/Class Lib XML/Class Lib XML/DataStore.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace Class_Lib_XML
+{
+    public class DataStore
+    {
+        public XSD.Gunstore instans;
+        public DataStore()
+        {
+            using (var reader = XmlReader.Create("my.xml"))
+            {
+                var serializer = new XmlSerializer(typeof(XSD.Gunstore));
+
+                instans = (XSD.Gunstore)serializer.Deserialize(reader);
+            }
+        }
+    }
+}
---
+++ H2/Class Lib XML/ClassLibXMLTesst/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Class_Lib_XML;
+
+namespace ClassLibXMLTesst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataStore data1 = new DataStore();
+        }
+    }
+}
